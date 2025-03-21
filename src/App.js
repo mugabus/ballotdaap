94526f2d4a7006fa546c2144eec9cf1ef624d9e3
mugaabus <fatakimugaruka91@gmail.com>
2025-03-21 21:18:54 +0300

    edit the app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mugabus<code>src/App.js</code> 
+          Mugabus 
         </p>
         
       </header>
